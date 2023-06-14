63d28a9901c05ccf98cb4fb76b30295d2130d9b0
YarinKeren <jarin4378@gmail.com>
2023-06-14 17:57:07 +0300

    portfolio

---
+++ app.js
@@ -0,0 +1,26 @@
+const button = document.querySelector("#nav-button"); // Hamburger Icon
+const menu = document.querySelector("#navbar-sticky"); // Menu
+// Get all the buttons in the cards
+const buttons = document.querySelectorAll("button[name]");
+
+button.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+// Add click event listener to each button
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Get the name attribute of the clicked button
+    const name = button.getAttribute("name");
+    // Handle navigation based on the button name
+    if (name === "matrix") {
+      window.open("https://github.com/YarinKeren/MatrixCalculator");
+    } else if (name === "whatToWatch") {
+      window.open("https://github.com/YarinKeren/What-To-Watch");
+    } else if (name === "workoutTracker") {
+      window.open("https://github.com/YarinKeren/workout_app_android");
+    } else {
+      // Default action if no matching button name found
+    }
+  });
+});
